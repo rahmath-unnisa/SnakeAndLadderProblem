5c84d80b3b0e912da2b124757eb40e864bf6e7d8
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-09 22:40:31 +0530

    [Rahmath]Add: Player Checks For An Option

--- SnakeAndLadderProblems/Program.cs
+++ SnakeAndLadderProblems/Program.cs
@@ -1,5 +1,9 @@
-﻿using SnakeAndLadderProblems;
+﻿using System;
+using SnakeAndLadderProblems;
 
 SnakeAndLadderGame game = new SnakeAndLadderGame();
 game.DieRoll();
 Console.WriteLine(game.DieRoll());
+game.Player();
+Console.WriteLine(game.Player());
+
--- SnakeAndLadderProblems/SnakeAndLadderGame.cs
+++ SnakeAndLadderProblems/SnakeAndLadderGame.cs
@@ -8,12 +8,34 @@ namespace SnakeAndLadderProblems
 {
     public class SnakeAndLadderGame
     {
-        int position = 0;
+        const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        int position = 0, option; 
         public int DieRoll()
         {
             Random random = new Random();
             int diePosition = random.Next(1, 7);
             return diePosition;
         }
+        public void Player()
+        {
+            Random random = new Random();
+            option = random.Next(0, 3);
+            switch (option)
+            {
+                case NO_PLAY:
+                    this.position = this.position;
+                    break;
+                case SNAKE:
+                    this.position -= this.DieRoll();
+                    break;
+                default :
+                    LADDER: this.position += this.DieRoll();
+                    break;
+            }
+
+
+            
+        }
     }
+    
 }
